18dfa2284e05ec1a218b1832500593db86430d6b
apurvsingh <apurvsingh95@outlook.com>
2022-02-04 19:42:04 -0500

    Init, Navbar, Slider

---
+++ src/components/Announcements.jsx
@@ -0,0 +1,20 @@
+import styled from 'styled-components';
+
+const Container = styled.div`
+    height: 30px;
+    background-color: teal;
+    color:white;
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    font-size: 14px;
+    font-weight: 500;
+`
+
+export const Announcements = () => {
+  return (<Container>
+      Super Deal! Free Shipping on orders above $50!
+  </Container>);
+};
+
+export default Announcements;

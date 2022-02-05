1c3fadfec63b61bc7ab3a0300babb2ec9ba8a6ed
apurvsingh <apurvsingh95@outlook.com>
2022-02-04 22:30:34 -0500

    Newsletter front-end only

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -4,6 +4,8 @@ import Announcements from '../components/Announcements';
 import Slider from '../components/Slider';
 import Catergories from '../components/Catergories';
 import Products from '../components/Products';
+import Newsletter from '../components/Newsletter';
+
 
 const Home = () => {
   return (
@@ -13,6 +15,7 @@ const Home = () => {
         <Slider/>
         <Catergories></Catergories>
         <Products></Products>
+        <Newsletter></Newsletter>
     </div>);
   
 };

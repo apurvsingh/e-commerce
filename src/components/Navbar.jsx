2b0e16de7f086e3cb6b92bcb0620c78841fa82bf
apurvsingh <apurvsingh95@outlook.com>
2022-02-06 19:33:14 -0500

    Making layout mobile-responsive

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -2,15 +2,18 @@ import { Badge } from '@material-ui/core';
 import { Search, ShoppingCartOutlined } from '@material-ui/icons';
 import React from 'react';
 import styled from 'styled-components';
+import {mobile} from '../responsive';
 
 const Container = styled.div`
     height: 60px;
-    
+    ${mobile({ height: "50px" })}
 `
 const Wrapper = styled.div`
     padding: 10px 20px;
     display: flex;
     justify-content: space-between;
+    ${mobile({ padding: "10px 0px" })}
+    })
 `
 
 const Left = styled.div`
@@ -31,11 +34,13 @@ const Right = styled.div`
     display: flex;
     align-items: center;
     justify-content: flex-end;
+    ${mobile({ flex: 2, justifyContent: "center" })}
 `
 
 const Language = styled.div`
     font-size: 14px;
     cursor: pointer;
+    ${mobile({ display: "none" })}
 `
 
 const SearchContainer = styled.div`
@@ -48,16 +53,19 @@ const SearchContainer = styled.div`
 
 const Input = styled.input`
     border: none;
+    ${mobile({ width: "50px" })}
 `
 
 const Logo = styled.h1`
     font-weight: bold;
+    ${mobile({ fontSize: "20px" })}
 `
 
 const MenuItem = styled.div`
     font-size:14px;
     cursor: pointer;
     margin-left: 25px;
+    ${mobile({ fontSize: "12px", marginLeft: "10px" })}
 `
 
 const Navbar = () => {
